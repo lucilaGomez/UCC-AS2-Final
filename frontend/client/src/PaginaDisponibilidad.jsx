7d2188b93ae85d191858083b00ce9d89ddb5d97d
mquelas <maxiquelas80@gmail.com>
2023-11-06 17:15:42 -0300

    hola

--- frontend/client/src/PaginaDisponibilidad.jsx
+++ frontend/client/src/PaginaDisponibilidad.jsx
@@ -1,26 +1,48 @@
-import React from "react";
-import { Link } from "react-router-dom";
-    
-    
-    const navbarLinks = [
 
-    { url: "/", title: "Reservar" },
-    { url: "/misreservas", title: "Mis Reservas" },
-    { url: "/login", title: "Login" },
-      ];
-      
-      
-      
-      const DisponibilidadnReserva = () => {
-        return (
-          <div className="confirmacion-reserva">
-                <Link to="/">
-              <button>Volver al menu principal</button>
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const Inforeserva = () => {
+    const [hotels, setHotels] = useState([]);
+
+    useEffect(() => {
+        const fetchHotels = async () => {
+            try {
+                const response = await axios.get("http://localhost:8080/hotel");
+                setHotels(response.data);
+            } catch (error) {
+                console.error("Error al obtener datos del servidor:", error);
+            }
+        };
+
+        fetchHotels();
+
+    }, []);
+
+    return (
+        <div className="confirmacion-reserva">
+            <div className="hotels-container">
+                {hotels.length ? (
+                    hotels.map((hotel) => (
+                        <div className="hotel-card" key={hotel.id}>
+                            <h4>{hotel.name}</h4>
+                            <p>Cantidad de Habitaciones: {hotel.roomAmount}</p>
+                            <p>Descripción: {hotel.description}</p>
+                            <p>Ciudad: {hotel.city}</p>
+                            <p>Dirección: {hotel.streetName} {hotel.streetNumber}</p>
+                            <p>Tarifa: {hotel.rate}</p>
+                        </div>
+                    ))
+                ) : (
+                    <p>No hay hoteles disponibles.</p>
+                )}
+            </div>
+            <Link to="/">
+                <button>Volver al menú principal</button>
             </Link>
-          </div>
-          
-        );
-      };
-      
-      export default DisponibilidadnReserva;
-      
+        </div>
+    );
+};
+
+export default Inforeserva;
--- frontend/client/src/PaginaInfohoteles.jsx
+++ frontend/client/src/PaginaInfohoteles.jsx
@@ -1,39 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
 
-const Inforeserva = () => {
-  const [hotels, setHotels] = useState([]);
+const navbarLinks = [
+  { url: "/", title: "Reservar" },
+  { url: "/misreservas", title: "Mis Reservas" },
+  { url: "/login", title: "Login" },
+];
 
-  useEffect(() => {
-    const fetchHotels = async () => {
-      try {
-        const response = await axios.get("http://localhost:8080/hotel");
-        setHotels(response.data);
-      } catch (error) {
-        console.error("Error al obtener datos del servidor:", error);
-      }
-    };
-
-    fetchHotels();
-
-  }, []);
-
+const DisponibilidadnReserva = () => {
   return (
     <div className="confirmacion-reserva">
-      <div className="hotels-container">
-        {hotels.length ? (
-          hotels.map((hotel) => (
-            <div className="hotel-card" key={hotel.id}>
-              <h4>{hotel.nombre}</h4>
-              <p>{hotel.email}</p>
-              <p>Descripción: {hotel.descripcion}</p>
-            </div>
-          ))
-        ) : (
-          <p>No hay hoteles disponibles.</p>
-        )} 
-      </div>
+      <h2>Informacion de su hotel</h2>
       <Link to="/">
         <button>Volver al menú principal</button>
       </Link>
@@ -41,4 +18,4 @@ const Inforeserva = () => {
   );
 };
 
-export default Inforeserva;
+export default DisponibilidadnReserva;
